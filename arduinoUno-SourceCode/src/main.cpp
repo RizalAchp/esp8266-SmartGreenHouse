431b226efc73219109197085b63e0b3c8c7f7776
RizalAchp <--null>
2022-02-02 18:09:55 +0700

    menambahkan penjelasan

--- arduinoUno-SourceCode/src/main.cpp
+++ arduinoUno-SourceCode/src/main.cpp
@@ -1,21 +1,31 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
+
+// include library yang dibutuhkan untuk encode json yang akan
+// dikirimkan melaluui software serial
+#include <ArduinoJson.h>
+
+// include library yang dibutuhkan untuk sensor
 #include <Ultrasonic.h>
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
-#include <ArduinoJson.h>
 
+// pin software serial pada arduino yang disambungkan pada esp
+#include <SoftwareSerial.h>
 SoftwareSerial espSerial(2,3);
+
+// define pin pin uang dibutuhkan pada arduino
 #define pinTRIG   9
 #define pinECHO   10
 #define pinSOIL   0
 #define pinDHT    11
 #define pinLDR    2
-const int relay[] = {4,5,6,7};
-const unsigned long interval = 1000;
+const int relay[] = {4,5,6,7}; // pin pin yang dibutuhkan untuk keempat relay
+const unsigned long interval = 1000; // interval waktu atau pengganti `delay()` pada loop
 
 DHT dht(pinDHT, DHT11);
 Ultrasonic hcsr(pinTRIG, pinECHO);
+
+// global varibale untuk data bacaaan sensor
 long  val_ldr;   
 long  val_soil;   
 int   val_hcsr;   
@@ -24,7 +34,9 @@ float val_dhttemp;
 
 
 void setup() {
+  // setup serial
   espSerial.begin(115200);
+  // mengubah mode pin relay menjadi output
   for (int i = 0; i < 4; i++){
     pinMode(relay[i], OUTPUT);
   }
@@ -32,9 +44,11 @@ void setup() {
 }
 
 void loop() {
+  // menggantikan `delay()` menggunakan millis agar tidak blocking code
   const unsigned long mills = millis();
   unsigned long now = 0;
   if ((unsigned long)(mills - now) > interval){
+    // semua perkondisian sensor pada relay
     val_dhthumid = dht.readHumidity(false);
     if ( val_dhthumid >= 80.0){ digitalWrite(relay[0], LOW);
     } else if ( val_dhthumid <= 80.0){
@@ -58,6 +72,9 @@ void loop() {
     } else if ( val_soil <= 30){ digitalWrite(relay[3], LOW); }
     val_ldr = map(analogRead(pinLDR), 0, 1024, 0, 100);
 
+    // membuat json dinamic dokumen yang isinya semua value sensor
+    // yang sudah di inisialisasikan menjadi json tersebut dan dikirimkan
+    // pada software serial dan akan di terima oleh ESP8266
     DynamicJsonDocument docs(1024);
     docs["humid"] = val_dhthumid;
     docs["temp"] = val_dhttemp;
--- esp8266-SourceCode/src/main.cpp
+++ esp8266-SourceCode/src/main.cpp
@@ -8,52 +8,60 @@
 #include <ArduinoJson.h>
 
 
-//TODO! menerima data dari arduino dan  memproses data tersebut sehingga dapat digunakan
-//      untuk ditampilkan pada WEB!
+// global variable data sensor yang diterima pada json docs
+// dari serial arduino
 long soil   ;
 long ldr    ;
 int hcsr    ;
 float humid ;
 float temp  ;
 
-const char* ssid = "Bismillah";
-const char* pswd = "MuhammadSAW";
-
+// masukkan ssid dan password wifi anda dibawah ini
+const char* ssid = "WIFI_ANDA";
+const char* pswd = "PASSWORD_WIFI_ANDA";
+// membuatserver pada port 80
 AsyncWebServer server(80);
 
 void setup(){
   Serial.begin(115200);
   
+  // menggunakan SPIFFS sebagai tempat index.html yang berada pada
+  // folder data didalam projek
   if(!SPIFFS.begin()){ return; }
   WiFi.begin(ssid, pswd);
   while (WiFi.status() != WL_CONNECTED){
     delay(1000);
   }
+  // menampilkan ip address yang di dapatkan oleh esp
   Serial.println(WiFi.localIP());
 
+  // http get 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html");
   });
   server.on("/humid", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(humid).c_str());
+    request->send_P(200, "text/plain", String(humid).c_str()); // mengirim data humid menjadi String
   });
   server.on("/temp", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(temp).c_str());
+    request->send_P(200, "text/plain", String(temp).c_str()); // megirim data temp menjadi String
   });
   server.on("/hcsr", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(hcsr).c_str());
+    request->send_P(200, "text/plain", String(hcsr).c_str()); // mengirim data hcsr menjadi Stringg
   });
   server.on("/soil", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(soil).c_str());
+    request->send_P(200, "text/plain", String(soil).c_str()); // mengirim data soil menjadi String
   });
   server.on("/ldr", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(ldr).c_str());
+    request->send_P(200, "text/plain", String(ldr).c_str()); // mengirim data ldr menjadi String
   });
 
-  server.begin();
+  server.begin(); // memulai server tersebut
 }
 
 void loop(){
+  // pada loop hanya memeriksa apakah ada data yang diterima di serial
+  // jika tersedia maka akan di encode menggunakan json dan memasukkan value sensor pada
+  // variable yang sudah disiapkan diatas.
   if (Serial.available()){
     DynamicJsonDocument docs(1024);
     DeserializationError error = deserializeJson(docs, Serial);
